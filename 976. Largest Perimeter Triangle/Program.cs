bf06fabf7b5bb211cc25c4c736d777cc9fd6ad75
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-03-01 15:04:13 +0500

    976 problem solved

--- 976. Largest Perimeter Triangle/Program.cs
+++ 976. Largest Perimeter Triangle/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+int[] nums = { 1, 2, 3, 4, 5, 6 };
+
+var res = new Solution();
+var result = res.LargestPerimeter(nums);
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int LargestPerimeter(int[] nums)
+    {
+        Array.Sort(nums);
+        for (int i = nums.Length - 1; i >= 2; i--)
+            if (nums[i] < nums[i - 1] + nums[i - 2])
+                return nums[i] + nums[i - 1] + nums[i - 2];
+        return 0;
+    }
+}

cf5a2fb22a8a09a94a19f4d766da984e5bac43a7
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-03-01 15:22:58 +0500

    1154. Day of the Year solved

---
+++ 1154. Day of the Year/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string test = "2020-12-31";
+
+var res = new Solution();
+Console.WriteLine(res.DayOfYear(test));
+
+
+public class Solution
+{
+    public int DayOfYear(string date)
+    {
+        int year = int.Parse(date[..4]);
+        int month = int.Parse(date[5..7]);
+        int day = int.Parse(date[8..]);
+
+        int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            days[1] = 29;
+
+        int result = 0;
+        for (int i = 0; i < month - 1; i++)
+            result += days[i];
+
+        return result + day;
+    }
+}

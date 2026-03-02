711fc3e175bbe1926bdd070b2a9058dd30dded7c
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-03-02 22:53:51 +0500

    507. Perfect Number added

---
+++ 507. Perfect Number/Program.cs
@@ -0,0 +1,23 @@
+﻿var num = 28;
+var misol1 = new Solution();
+Console.WriteLine(misol1.CheckPerfectNumber(num));
+
+
+public class Solution
+{
+    public bool CheckPerfectNumber(int num)
+    {
+        if (num <= 1) return false;
+        int sum = 1;
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                sum += i;
+                if (i != num / i)
+                    sum += num / i;
+            }
+        }
+        return sum == num;
+    }
+}

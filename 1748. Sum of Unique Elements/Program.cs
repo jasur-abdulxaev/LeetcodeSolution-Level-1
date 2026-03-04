9730c385e9859c36c32b9a30035286157d06eb1d
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-03-04 23:54:37 +0500

    1748 and 819 tasks added letcode

---
+++ 1748. Sum of Unique Elements/Program.cs
@@ -0,0 +1,20 @@
+﻿
+var result = new Solution().SumOfUnique(new int[] { 1, 2, 3, 2 });
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int SumOfUnique(int[] nums)
+    {
+        // Count frequency using array since nums[i] is between 1 and 100
+        int[] freq = new int[101];
+        foreach (int num in nums) freq[num]++;
+
+        //Sum elements that appear exactly once
+        int sum = 0;
+        for (int i = 1; i <= 100; i++)
+            if (freq[i] == 1) sum += i;
+
+        return sum;
+    }
+}
---
+++ 819. Most Common Word/Program.cs
@@ -0,0 +1,35 @@
+﻿
+var result = new Solution().MostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.",
+    new string[] { "hit" });
+Console.WriteLine(result);
+
+
+public class Solution
+{
+    public string MostCommonWord(string paragraph, string[] banned)
+    {
+        var bannedSet = new HashSet<string>(banned);
+        var words = paragraph.ToLower().Split(new char[] { ' ', '!', '?', '\'', ',', ';', '.' },
+            StringSplitOptions.RemoveEmptyEntries);
+
+        var freq = new Dictionary<string, int>();
+        string result = "";
+        int maxCount = 0;
+
+        foreach (var word in words)
+        {
+            if (bannedSet.Contains(word))
+                continue;
+
+            freq.TryGetValue(word, out int count);
+            freq[word] = count + 1;
+
+            if (count + 1 > maxCount)
+            {
+                maxCount = count + 1;
+                result = word;
+            }
+        }
+        return result;
+    }
+}

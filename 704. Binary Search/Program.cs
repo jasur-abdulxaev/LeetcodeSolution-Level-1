272ca2b464be568e212a3fedc05a15b8ff66e3c4
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-03-05 23:41:46 +0500

    704 Binary Search task added

---
+++ 704. Binary Search/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var result = new Solution().Search(new[] { -1, 0, 3, 5, 9, 12 }, 9);
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+}

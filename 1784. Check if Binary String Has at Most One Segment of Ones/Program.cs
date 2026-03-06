2d2977dc796e723c1e06a9be0cfb1ec6210266c5
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-03-06 23:58:19 +0500

    string tasks added

---
+++ 1784. Check if Binary String Has at Most One Segment of Ones/Program.cs
@@ -0,0 +1,11 @@
+﻿
+var result = new Solution().CheckOnesSegment("1001");
+Console.WriteLine(result);
+
+public class Solution
+{
+    public bool CheckOnesSegment(string s)
+    {
+        return !s.Contains("01");
+    }
+}
---
+++ 415. Add Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿
+var result = new Solution().AddStrings("123", "456");
+Console.WriteLine(result);
+
+public class Solution
+{
+    public string AddStrings(string num1, string num2)
+    {
+        int i = num1.Length - 1, j = num2.Length - 1, carry = 0;
+        var result = new System.Text.StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry > 0)
+        {
+            int x = i >= 0 ? num1[i--] - '0' : 0;
+            int y = j >= 0 ? num2[j--] - '0' : 0;
+
+            int sum = x + y + carry;
+            result.Append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return new string(result.ToString().Reverse().ToArray());
+    }
+}
